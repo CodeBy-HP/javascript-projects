e5ad08cc4b676cf56e8d90d35a573f4bf7c8a770
Harsh Patel <code.by.hp@gmail.com>
2025-11-03 05:19:50 +0530

    No functionality changes

--- clock-project/script.js
+++ clock-project/script.js
@@ -1,21 +1,34 @@
 let hr = document.querySelector("#hour");
-let min = document.querySelector("#min");
 let sec = document.querySelector("#sec");
+let min = document.querySelector("#min");
 
-function displayTime() {
+const displayTime = () => {
   let date = new Date();
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
 
-  let hh = date.getHours();
-  let mm = date.getMinutes();
-  let ss = date.getSeconds();
-
-  let hRotation = 30 * hh + mm / 2;
-  let mRotation = 6 * mm;
-  let sRotation = 6 * ss;
-
-  hr.style.transform = `rotate(${hRotation}deg)`;
-  min.style.transform = `rotate(${mRotation}deg)`;
-  sec.style.transform = `rotate(${sRotation}deg)`;
-}
+  hr.style.transform = `rotate(${h * 30 + m * 0.5}deg)`;
+  min.style.transform = `rotate(${m * 6}deg)`;
+  sec.style.transform = `rotate(${s * 6}deg)`;
+};
 
 setInterval(displayTime, 1000);
+
+/*
+12 hr = 360deg
+1 hr = 30deg
+x hr = x * 30deg + m * 0.5deg
+
+60 min = 30deg
+1 min = 0.5deg
+m min = 0.5 * mdeg
+
+60 min = 360deg
+1 min = 6deg
+y min = y * 6deg
+
+60 sec = 360deg
+1 sec = 6deg
+z sec = z * 6deg
+*/
