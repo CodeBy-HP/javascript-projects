217aa8fc11d2073da0280d4746896e67e2b20136
Harsh Patel <code.by.hp@gmail.com>
2025-11-07 19:37:42 +0530

    Updated: clock-project_code

--- clock-project/script.js
+++ clock-project/script.js
@@ -1,34 +1,22 @@
-let hr = document.querySelector("#hour");
-let sec = document.querySelector("#sec");
-let min = document.querySelector("#min");
+const hourHand = document.querySelector(".hour");
+const minuteHand = document.querySelector(".minute");
+const secondHand = document.querySelector(".second");
 
-const displayTime = () => {
-  let date = new Date();
-  let h = date.getHours();
-  let m = date.getMinutes();
-  let s = date.getSeconds();
+function updateClock() {
+  const now = new Date();
 
-  hr.style.transform = `rotate(${h * 30 + m * 0.5}deg)`;
-  min.style.transform = `rotate(${m * 6}deg)`;
-  sec.style.transform = `rotate(${s * 6}deg)`;
-};
+  const h = now.getHours() % 12;
+  const m = now.getMinutes();
+  const s = now.getSeconds();
 
-setInterval(displayTime, 1000);
+  const hourDeg = 30 * h + 0.5 * m;
+  const minuteDeg = 6 * m;
+  const secondDeg = 6 * s;
 
-/*
-12 hr = 360deg
-1 hr = 30deg
-x hr = x * 30deg + m * 0.5deg
+  hourHand.style.transform = `translate(-50%, 0) rotate(${hourDeg}deg)`;
+  minuteHand.style.transform = `translate(-50%, 0) rotate(${minuteDeg}deg)`;
+  secondHand.style.transform = `translate(-50%, 0) rotate(${secondDeg}deg)`;
+}
 
-60 min = 30deg
-1 min = 0.5deg
-m min = 0.5 * mdeg
-
-60 min = 360deg
-1 min = 6deg
-y min = y * 6deg
-
-60 sec = 360deg
-1 sec = 6deg
-z sec = z * 6deg
-*/
+setInterval(updateClock, 1000);
+updateClock();
